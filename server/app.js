9ee803abaf9c11d9e1603c486790348b86075569
Oleg <shestakovoa@gmail.com>
2022-09-12 21:01:40 +0500

    serv

---
+++ server/app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const dbConnectionCheck = require('./db/dbConnectCheck');
+
+const { PORT, SESSION_SECRET } = process.env;
+
+const app = express();
+dbConnectionCheck();
+
+const corsOptions = {
+  credentials: true,
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
+
+// тут импорты всех роутов, если нужно
+// const indexRoutes = require('./routes/indexRoutes');
+// const loginRoutes = require('./routes/loginRoutes');
+// const regRoutes = require('./routes/regRoutes');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public/')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// КОНФИГ ДЛЯ КУКИ
+const sessionConfig = {
+  name: 'DoctorCookie', // * Название куки
+  store: new FileStore(), // * подключение стора (БД для куки) для хранения
+  secret: SESSION_SECRET ?? 'gkp5o34kg5j094gk940', // * ключ для шифрования куки
+  resave: false, // * если true, пересохраняет сессию, даже если она не поменялась
+  saveUninitialized: false, // * Если false, куки появляются только при установке req.session
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 10, // * время жизни в ms (10 дней)
+    httpOnly: true, // * куки только по http
+  },
+};
+// подключение мидлвара для куки
+app.use(session(sessionConfig));
+
+// ссылки на роуты
+// app.use('/', indexRoutes);
+// app.use('/login', loginRoutes);
+// app.use('/register', regRoutes);
+
+app.listen(PORT ?? 3100, () => {
+  console.log('Сервер запущен!');
+});
