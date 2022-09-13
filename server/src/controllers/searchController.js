527e4ef961c2484dd98f56d0f675e1840dabd138
Raian Girfanov <68778468+Watermelonish@users.noreply.github.com>
2022-09-14 00:24:17 +0300

    backend started

---
+++ server/src/controllers/searchController.js
@@ -0,0 +1,16 @@
+const {Doctor} = require('../../db/models')
+
+async function getDoctors (req, res)  {
+    try{
+        const {inputspecialist, inputplace} = req.params;
+        console.log(inputSpecialist, inputPlace)
+        const theDoctors = await Doctor.findAll()
+        console.log(theDoctors)
+        return res.json(theDoctors)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+
+module.exports = {getDoctors}
--- server/src/routers/seatchRouter.js
+++
@@ -1,6 +0,0 @@
-const {} = require("../controllers/contactsController")
-const router = require('express').Router();
-
-router.post("/sendemail", sendEmail);
-
-module.exports =router;
