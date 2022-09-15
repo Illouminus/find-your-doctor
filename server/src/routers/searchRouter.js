6014378f2e98efbdb81e3408997792ae8744911c
Raian Girfanov <68778468+Watermelonish@users.noreply.github.com>
2022-09-15 17:04:17 +0300

    backend ready

--- server/src/routers/searchRouter.js
+++ server/src/routers/searchRouter.js
@@ -1,6 +1,6 @@
 const {getDoctors} = require("../controllers/searchController")
 const router = require('express').Router();
 
-router.get("/main/:inputspecialist/:inputplace", getDoctors);
+router.get("/main/:inputspecialist/:inputplace?", getDoctors);
 
 module.exports = router;
