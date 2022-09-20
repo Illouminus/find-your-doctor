16225d19223e1459de1b4eff21d2edc3f9726912
Oleg <shestakovoa@gmail.com>
2022-09-20 11:41:10 +0500

    fix

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,6 +7,9 @@ import TagsCard from "./TagsCard/TagsCard";
 import SearchedDoctorCard from "./SearchedDoctorCard/SearchedDoctorCard";
 import DocInfo from "./DocInfo/DocInfo";
 import DocCalendar from "./DocCalendar/DocCalendar";
-import FileUpload from "./FileUpload/FileUpload"
-export { Layout, Navigation, ResponsiveAppBar, SearchBar, TagsCard, SearchedDoctorCard, DocInfo,  LoginForm, DocCalendar, FileUpload,Upload, Map, RatingDoc };
+import FileUpload from "./FileUpload/FileUpload";
+import Upload from "./UploadDocuments/Upload";
+import Map from "./Map/Map";
+import RatingDoc from "./Stars/Stars";
+export { Layout, Navigation, ResponsiveAppBar, SearchBar, TagsCard, SearchedDoctorCard, DocInfo,  LoginForm, DocCalendar, FileUpload, Upload, Map, RatingDoc };
 
